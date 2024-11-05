6bd444a0a3b89fbd03632b728a69571a0437356c
mauzer3358 <mauzer3358@gmail.com>
2024-11-05 13:12:44 +0300

    Add files via upload

---
+++ Module_14_2.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+# for i in range(1,11):
+#     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?,?,?,?)",
+#                    (f'User{i}', f'example{i}@gmail.com', f'{i*10}','1000'))
+#
+# cursor.execute("UPDATE Users SET balance = ? WHERE (id%2)", (500,) )
+#
+# for i in range(1,11,3):
+#     cursor.execute("DELETE FROM Users WHERE id = ?", (f'{i}',))
+
+
+
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
+
+cursor.execute("SELECT SUM(balance) FROM Users")
+total_balance = cursor.fetchone()[0]
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+
+print(total_balance/total_users)
+
+#cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+# users = cursor.fetchall()
+#
+# for i in range(len(users)):
+#     print(f' Имя: {users[i][0]} | Почта: {users[i][1]} | Возраст: {users[i][2]} | Баланс: {users[i][3]}')
+
+
+
+connection.commit()
+connection.close()
